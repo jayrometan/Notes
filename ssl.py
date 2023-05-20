406d816ab01b40b4caab5dc7a2d2577f424d5f7f
Jerome Tan <34192985+jayrometan@users.noreply.github.com>
2023-05-20 22:08:21 +0800

    Update ssl.py

--- ssl.py
+++ ssl.py
@@ -1,19 +1,9 @@
-import urllib3
+import requests
+import ssl
 
-def check_https(endpoint, port):
+def get_ssl_cert_expiry_date(endpoint):
     http = urllib3.PoolManager()
-    try:
-        response = http.request('GET', 'https://{}:{}'.format(endpoint, port))
-        if response.status_code == 200:
-            return True
-        else:
-            return False
-    except urllib3.exceptions.SSLError:
-        return False
-
-def get_ssl_cert_expiry_date(endpoint, port):
-    http = urllib3.PoolManager()
-    response = http.request('GET', 'https://{}:{}'.format(endpoint, port))
+    response = http.request('GET', 'https://{}'.format(endpoint))
     if response.status_code == 200:
         cert = response.headers['content-type'].split(';')[1].split('/')[1]
         expiry_date = '{}-{}-{}'.format(cert[0:4], cert[5:7], cert[8:10])
@@ -21,15 +11,14 @@ def get_ssl_cert_expiry_date(endpoint, port):
     else:
         return None
 
+def monitor_ssl_expiry(endpoints):
+    for endpoint in endpoints:
+        expiry_date = get_ssl_cert_expiry_date(endpoint)
+        if expiry_date is not None:
+            print('The SSL cert for {} expires on {}'.format(endpoint, expiry_date))
+        else:
+            print('The SSL cert for {} is not valid'.format(endpoint))
+
 if __name__ == '__main__':
-    endpoint = 'www.google.com'
-    port = 443
-
-    is_https = check_https(endpoint, port)
-    expiry_date = get_ssl_cert_expiry_date(endpoint, port)
-
-    if is_https:
-        print('The endpoint is HTTPS')
-        print('The SSL cert expiry date is {}'.format(expiry_date))
-    else:
-        print('The endpoint is not HTTPS')
+    endpoints = ['www.google.com', 'www.facebook.com', 'www.yahoo.com']
+    monitor_ssl_expiry(endpoints)
